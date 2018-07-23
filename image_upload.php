4c924b937e38c00ce1f9ff6b75bba71df52512f2
shivanitekriwal1 <shivanitekriwal1@gmail.com>
2018-07-23 15:18:23 +0530

    uploads image to server and database

---
+++ image_upload.php
@@ -0,0 +1,24 @@
+<?php
+
+$db = mysqli_connect('localhost', 'root', '', 'users detail');
+
+$folder="C:/xampp/htdocs/user login/image/";
+$upload_image = $folder . basename($_FILES[ "myimage" ][ "name" ]);
+$image_name = $_FILES["myimage"]["name"];
+
+move_uploaded_file($_FILES[ "myimage" ][ "tmp_name"], "$folder".$_FILES[ "myimage" ][ "name" ]);
+
+$insert_path="INSERT INTO pictures (image) VALUES ('$image_name')";
+$result = mysqli_query($db, $insert_path);
+if($result){
+	echo "uploaded";
+}
+else{
+	echo "not uploaded";
+}
+
+
+
+
+
+
